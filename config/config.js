92f3b3db323a5e829623d1f1991421364b12d718
Jeff Gaskill <mistergaskill@gmail.com>
2015-02-04 17:28:30 -0800

    Differentiate package version via name.

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 var app = window.app
-app.controller('EmailNotifierCtrl', ['$scope', function ($scope) {
+app.controller('LvEmailNotifierCtrl', ['$scope', function ($scope) {
 
-  $scope.config = $scope.pluginConfig('emailnotifier')
+  $scope.config = $scope.pluginConfig('lvemailnotifier')
 
   $scope.$watch('config.notify', function(newValue, oldValue) {
     if (newValue !== oldValue) {
       $scope.saving = true;
-      $scope.pluginConfig('emailnotifier', $scope.config, function () {
+      $scope.pluginConfig('lvemailnotifier', $scope.config, function () {
         $scope.saving = false
       })
     }
